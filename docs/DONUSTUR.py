1ede33654745074b98a691e59be9f20b75aa0455
Hilal Azca <ehazca@gmail.com>
2022-10-17 18:46:50 +0300

    update

--- docs/DONUSTUR.py
+++ docs/DONUSTUR.py
@@ -3,7 +3,6 @@ import os
 try:
     os.system("python -m PyQt5.uic.pyuic -x havaDurumu.ui -o ui_havaDurumu.py")
     #☻os.system("pyrcc5 icons.qrc -o icons_rc.py")
-    print("basarili")
 except Exception as e:
     print(e)
     pass
